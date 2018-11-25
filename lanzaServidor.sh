7ead7bcb62d1d00bd960cc8f44e74841be7c685e
Sheppardd <38540304+Sheppardd@users.noreply.github.com>
2018-11-25 17:18:42 +0100

    bash fixed >D

--- lanzaServidor.sh
+++ lanzaServidor.sh
@@ -1,21 +1,17 @@
-#!/bin/bash
+!/bin/bash
 
 serverID=$(pgrep server)
-if [-z $serverID]; then
+
+if [ -z $serverID ]; then
 	echo "There is no server up"
 else
 	kill $serverID
 fi
 
-rm cliente.txt
-rm server.txt
-rm ficherosTFTPserver/testFile.txt
-
+make clean &
 make server &
-make client &
-make clean
+make client
 
 ./server
 sleep 1
 ./client localhost TCP l testFile.txt
-
